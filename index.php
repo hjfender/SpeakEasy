ea9e84ee44845f3cbba2813999fa4ed4def4593b
unknown <needarb@gmail.com>
2015-03-11 11:09:48 -0500

    connor files

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">SpeakEasy</a>
+            <a class="navbar-brand" href="index.php">SpeakEasy</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
